0f9d2a607f6fc0e849b9e874284c07cfdfa46c27
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2018-12-06 17:53:07 +0400

    add solution for Reversed Sequence 8 Kyu

---
+++ Java/8_Kyu/ReversedSequence.java
@@ -0,0 +1,15 @@
+/*
+Original task - https://www.codewars.com/kata/reversed-sequence
+
+Get the number n (n>0) to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1]
+ */
+ 
+public class Sequence {
+    public static int[] reverse(int n) {
+        int[] reverse = new int[n];
+        for(; n > 0; n--) reverse[reverse.length - n] = n;
+        return reverse;
+    }
+}
