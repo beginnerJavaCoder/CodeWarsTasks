31f45ee5f7384570eeb4e8df4760ad11dda418d1
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2018-12-06 22:09:08 +0400

    add solution for Return negative (8 Kyu)

--- Java/8_Kyu/ReturnNegative.java
+++ Java/8_Kyu/ReturnNegative.java
@@ -11,7 +11,7 @@ Kata.makeNegative(0); // return 0
 
 Notes:
 The number can be negative already, in which case no change is required.
-Zero (0) is not checked for any specific sign.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
  */
  
 public class Kata {
