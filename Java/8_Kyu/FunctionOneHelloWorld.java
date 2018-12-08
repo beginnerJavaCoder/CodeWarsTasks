77e5bcc0634b72f44ff07652817929c902723299
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2018-12-08 23:21:37 +0400

    add solution for Function 1 - Hello World (8 Kyu)

---
+++ Java/8_Kyu/FunctionOneHelloWorld.java
@@ -0,0 +1,12 @@
+/*
+Original task - https://www.codewars.com/kata/function-1-hello-world
+
+Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+ */
+ 
+public class HelloWorld {
+    public static String greet() {
+        return "hello world!"; 
+    }
+}
