35312766af8ee46094eb8dabc2d8e93f9ae5cf76
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2018-12-10 19:08:37 +0400

    add solution for Weight for Weight (5 Kyu)

--- Java/5_Kyu/WeightForWeight.java
+++ Java/5_Kyu/WeightForWeight.java
@@ -23,3 +23,91 @@ It may happen that the input string have leading, trailing whitespaces and more 
 Don't modify the input.
  */
  
+import java.util.ArrayList;
+
+public class WeightSort {
+
+    public static String orderWeight(String string) {
+        String[] values = parseString(string);
+        int[] weights = getWeights(values);
+        sortArraysByWeight(values, weights);
+        String result = getFormatString(values);
+
+        return result;
+    }
+
+    private static boolean isDigit(int codePoint) {
+        return codePoint > 47 && codePoint < 58;
+    }
+
+    private static int[] getWeights(String[] values) {
+        int[] byWeights = new int[values.length];
+        for (int i = 0; i < byWeights.length; i++) {
+            int weight = 0;
+            for (int j = 0; j < values[i].length(); j++) {
+                weight += Integer.parseInt(values[i].charAt(j) + "");
+            }
+            byWeights[i] = weight;
+        }
+
+        return byWeights;
+    }
+
+    private static String[] parseString(String string) {
+        ArrayList<String> values = new ArrayList<>();
+        StringBuilder tmp = new StringBuilder();
+        int length = string.length();
+        for (int i = 0; i < length; i++) {
+            while (i < length && isDigit(string.codePointAt(i)) ) {
+                tmp.append(string.charAt(i));
+                i++;
+            }
+            if(!tmp.toString().equals("")) {
+                values.add(tmp.toString());
+                tmp = new StringBuilder();
+            }
+        }
+        String[] strValues = new String[values.size()];
+		
+        return values.toArray(strValues);
+    }
+
+    private static void sortArraysByWeight(String[] values, int[] weights) {
+        for (int i = 0; i < weights.length - 1; i++) {
+            int indexMin = i;
+            int min = weights[indexMin];
+            for (int j = i + 1; j < weights.length; j++) {
+                if (weights[j] == min) {
+                    if (values[j].compareTo(values[indexMin]) < 0) {
+                        min = weights[j];
+                        indexMin = j;
+                    }
+                }
+                if (weights[j] < min) {
+                    min = weights[j];
+                    indexMin = j;
+                }
+            }
+
+            int tmp = weights[i];
+            weights[i] = min;
+            weights[indexMin] = tmp;
+            String tmpStr = values[i];
+            values[i] = values[indexMin];
+            values[indexMin] = tmpStr;
+        }
+    }
+
+    private static String getFormatString(String[] array) {
+        if(array.length == 0) return "";
+        StringBuilder result = new StringBuilder();
+
+        for (String s : array) {
+            result.append(s);
+            result.append(" ");
+        }
+        result.deleteCharAt(result.length()-1);
+
+        return result.toString();
+    }
+}
