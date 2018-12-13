6b950422587861461453800263be717cbecc0cd0
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2018-12-13 18:22:56 +0400

    add solution for Sum of positive (8 Kyu)

---
+++ Java/8_Kyu/SumOfPositive.java
@@ -0,0 +1,17 @@
+/*
+Original task - https://www.codewars.com/kata/sum-of-positive
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+ */
+ 
+public class Positive {
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for(int num : arr) if(num > 0) sum += num;
+        return sum;
+    }
+}
