6713f5c555bc59f8189bbbc74e86e36ce0f67dad
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2018-12-30 20:05:15 +0400

    add solution for All Star Code Challenge #18 (8 Kyu)

--- Java/8_Kyu/AllStarCodeChallenge#18.java
+++ Java/8_Kyu/AllStarCodeChallenge#18.java
@@ -3,7 +3,6 @@ Original task - https://www.codewars.com/kata/all-star-code-challenge-number-18
 
 Create a function called that accepts 2 string arguments and returns 
 an integer of the count of occurrences the 2nd argument is found in the first one.
-
 If no occurrences can be found, a count of 0 should be returned.
 
 strCount("Hello", 'o') // => 1
