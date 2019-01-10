33cf26862ec88cb58e2782fe0966e10162c52e6c
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2019-01-10 16:48:18 +0400

    add solution for Tidy Number (Special Numbers Series #9) (7 Kyu)

---
+++ Java/7_Kyu/TidyNumber.java
@@ -0,0 +1,47 @@
+/*
+Original task - https://www.codewars.com/kata/tidy-number-special-numbers-series-number-9
+
+Definition
+A Tidy number is a number whose digits are in non-decreasing order.
+
+Task
+Given a number, find if it is Tidy or not.
+
+Notes
+Number passed is always Positive.
+Return the result as a Boolean.
+
+Input >> Output Examples
+1- tidyNumber (12) ==> return (true)
+Explanation:
+The number's digits { 1 , 2 } are in non-Decreasing Order (i.e) 1 <= 2.
+
+2- tidyNumber (32) ==> return (false)
+Explanation:
+The Number's Digits { 3, 2} are not in non-Decreasing Order (i.e) 3 > 2.
+
+3- tidyNumber (1024) ==> return (false)
+Explanation:
+The Number's Digits {1 , 0, 2, 4} are not in non-Decreasing Order as 0 <= 1.
+
+4- tidyNumber (13579) ==> return (true)
+Explanation:
+The number's digits {1 , 3, 5, 7, 9} are in non-Decreasing Order.
+
+5- tidyNumber (2335) ==> return (true)
+Explanation:
+The number's digits {2 , 3, 3, 5} are in non-Decreasing Order, Note 3 <= 3
+ */
+ 
+public class Solution {
+    public static boolean tidyNumber(int number) {
+        String num = "" + number;
+        for (int i = 1; i < num.length(); i++) {
+            if (num.codePointAt(i) < num.codePointAt(i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
