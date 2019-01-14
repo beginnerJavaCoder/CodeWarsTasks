3ffddc1ab3ddfad10edda9e8600fc4c9c0fbfea8
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2019-01-14 20:09:06 +0400

    add solution for You Can't Code Under Pressure #1 (8 Kyu)

---
+++ Java/8_Kyu/YouCantCodeUnderPressure#1.java
@@ -0,0 +1,11 @@
+/*
+Original task - https://www.codewars.com/kata/you-cant-code-under-pressure-number-1
+
+Code as fast as you can! You need to double the integer and return it.
+ */
+ 
+class Java {
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
