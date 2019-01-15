b0981417c46da926937332e3c8f805fc54e22b33
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2019-01-15 22:30:56 +0400

    add solution for Opposite number (8 Kyu)

---
+++ Java/8_Kyu/OppositeNumber.java
@@ -0,0 +1,17 @@
+/*
+Original task - https://www.codewars.com/kata/opposite-number
+
+Very simple, given a number, find its opposite.
+
+Examples:
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+ */
+ 
+public class Kata {
+    public static int opposite(int number) {
+        return -number;
+    }
+}
