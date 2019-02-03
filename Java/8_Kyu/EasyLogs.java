8dca785966e3e25911b6df4dca8cb3b32a2af5eb
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2019-02-03 20:56:51 +0400

    add solution for easy logs (8 Kyu)

---
+++ Java/8_Kyu/EasyLogs.java
@@ -0,0 +1,12 @@
+/*
+Original task - https://www.codewars.com/kata/easy-logs
+
+Add two logs with base X, with the value of A and B. 
+Example log A + log B where the base is X.
+ */
+ 
+public class EasyLogs {
+    public static double logs(double x, double a, double b) {
+        return Math.log(a * b) / Math.log(x);
+    }
+}
