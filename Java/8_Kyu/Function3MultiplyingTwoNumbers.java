8f3a8162cc84ab3ce26b6b7bf2b0a6d54dad80e6
Eugene Esipov <zhenyaesipovm@mail.ru>
2019-04-03 21:19:48 +0300

    add solution for Function 3 - multiplying two numbers (8 Kyu)

---
+++ Java/8_Kyu/Function3MultiplyingTwoNumbers.java
@@ -0,0 +1,12 @@
+/*
+Original task - https://www.codewars.com/kata/function-3-multiplying-two-numbers
+
+This function has to be called multiply and needs to take two numbers as arguments, 
+and has to return the multiplication of the two arguments.
+ */
+ 
+public class Kata {
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    } 
+}
