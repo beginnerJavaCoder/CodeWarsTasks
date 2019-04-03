6bc6a4da153ddc09e13a9ae131e64f07469880ec
Eugene Esipov <zhenyaesipovm@mail.ru>
2019-04-03 21:13:22 +0300

    add solution for Fake Binary (8 Kyu)

---
+++ Java/8_Kyu/FakeBinary.java
@@ -0,0 +1,12 @@
+/*
+Original task - https://www.codewars.com/kata/fake-binary
+
+Given a string of digits, you should replace any digit below 5 with '0'
+and any digit 5 and above with '1'. Return the resulting string.
+ */
+ 
+public class FakeBinary {
+    public static String fakeBin(String numberString) {
+        return numberString.replaceAll("[0-4]", "0").replaceAll("[5-9]", "1");
+    }
+}
