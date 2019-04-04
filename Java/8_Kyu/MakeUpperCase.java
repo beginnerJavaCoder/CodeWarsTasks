0608be1fa5712b399b7cb2dbdc40e3d1341920ac
Eugene Esipov <zhenyaesipovm@mail.ru>
2019-04-04 20:20:17 +0300

    add solution for MakeUpperCase (8 Kyu)

---
+++ Java/8_Kyu/MakeUpperCase.java
@@ -0,0 +1,11 @@
+/*
+Original task - https://www.codewars.com/kata/makeuppercase
+
+Write function makeUpperCase.
+ */
+ 
+class Upper {
+    public static String MakeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
