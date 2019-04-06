39292fb828c207bb79ae61e0671adff000896296
Eugene Esipov <zhenyaesipovm@mail.ru>
2019-04-06 23:21:02 +0300

    add solution for Number toString (8 Kyu)

---
+++ Java/8_Kyu/NumberToString.java
@@ -0,0 +1,13 @@
+/*
+Original task - https://www.codewars.com/kata/number-tostring
+
+The code gives an error!
+
+public static final String a = 123.toString();
+
+Fix it!
+ */
+ 
+public class ToString {
+    public static final String a = 123 + "";
+}
