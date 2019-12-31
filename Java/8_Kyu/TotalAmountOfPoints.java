c64007dea4b012755fda6b2512997616e07aa5fb
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2020-01-01 00:00:17 +0400

    add solution for Total amount of points (8 Kyu)

---
+++ Java/8_Kyu/TotalAmountOfPoints.java
@@ -0,0 +1,38 @@
+/*
+Original task - https://www.codewars.com/kata/total-amount-of-points
+
+Our football team finished the championship. 
+The result of each match look like "x:y". 
+Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. 
+
+Rules for counting points for each match:
+
+if x>y - 3 points
+if x<y - 0 point
+if x=y - 1 point
+
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+ */
+ 
+public class TotalPoints {
+    public static int points(String[] games) {
+        int countOfPoints = 0;
+        for(int i = 0; i < games.length; i++) {
+            if (games[i].charAt(0) > games[i].charAt(2)) {
+                countOfPoints += 3;
+            } else if(games[i].charAt(0) == games[i].charAt(2)) {
+                countOfPoints += 1;
+            }
+        }
+        
+        return countOfPoints;
+    }
+}
