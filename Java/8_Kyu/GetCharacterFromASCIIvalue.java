e9c1537bf9c3d0a2983e36563b902c7276a734d4
beginnerJavaCoder <zhenyaesipovm@mail.ru>
2020-01-01 00:32:38 +0400

    add solution for get character from ASCII Value (8 Kyu)

---
+++ Java/8_Kyu/GetCharacterFromASCIIvalue.java
@@ -0,0 +1,15 @@
+/*
+Original task - https://www.codewars.com/kata/get-character-from-ascii-value
+
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+Ascii.getChar(65) // => 'A'
+ */
+ 
+public class Ascii {
+    public static char getChar(int c) {
+        return (char) c;
+    }
+}
