5f0b4d96a4fb3312375ea76057995235e70f197b
Eugene Esipov <zhenyaesipovm@mail.ru>
2020-05-10 23:40:30 +0400

    add solution for What's Your Poison? (6 Kyu)

---
+++ Java/6_Kyu/WhatsYourPoison.java
@@ -0,0 +1,53 @@
+/*
+Original task - https://www.codewars.com/kata/58c47a95e4eb57a5b9000094
+
+The Riddle
+
+The King of a small country invites 1000 senators to his annual party. 
+As a tradition, each senator brings the King a bottle of wine. 
+Soon after, the Queen discovers that one of the senators is trying 
+to assassinate the King by giving him a bottle of poisoned wine. 
+Unfortunately, they do not know which senator, 
+nor which bottle of wine is poisoned, and the poison is completely indiscernible.
+
+However, the King has 10 lab rats. 
+He decides to use them as taste testers to determine which bottle of wine contains the poison. 
+The poison when taken has no effect on the rats, until exactly 24 hours later when the infected rats suddenly die. 
+The King needs to determine which bottle of wine is poisoned by tomorrow, 
+so that the festivities can continue as planned.
+
+Hence he only has time for one round of testing, 
+he decides that each rat tastes multiple bottles, according to a certain scheme.
+
+Your Task
+
+You receive an array of integers (0 to 9), 
+each of them is the number of a rat which died after tasting the wine bottles. 
+Return the number of the bottle (1..1000) which is poisoned.
+
+Good Luck!
+
+Hint: think of rats as a certain representation of the number of the bottle...
+ */
+ 
+import java.util.Arrays;
+
+public class FindThePoison {
+    public int find(int[] rats) {
+        Arrays.sort(rats);
+        StringBuilder binaryString = new StringBuilder();
+        int arrIndex = 0;
+        for(int i = 0; i < 10; i++) {
+            if(rats[arrIndex] == i) {
+                binaryString.append("1");
+                if(arrIndex < rats.length - 1) {
+                    arrIndex++;
+                } else break;
+            } else {
+                binaryString.append("0");
+            }
+        }
+        
+        return Integer.parseInt(binaryString.reverse().toString(), 2);
+    }
+}
